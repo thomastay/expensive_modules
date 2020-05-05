12acf18142350c5965491de6f5948d4a44f4f39b
ttay <ttay@umich.edu>
2020-05-06 00:46:55 +0800

    realized loop is in wrong order

--- fsharp.Lib/TransitiveReduction.fs
+++ fsharp.Lib/TransitiveReduction.fs
@@ -10,11 +10,11 @@ let reduce (graph: Digraph): Digraph =
     let adj = graph.adjacency
     let high = Array2D.length1 adj - 1
     let mutable counter = 0
-    for i in 0..high do
-        for j in 0..high do
-            for k = high downto 0 do
+    for k = high downto 0 do
+        for i in 0..high do
+            for j in 0..high do
                 if adj.[i, k] && adj.[k, j] then
-                    counter <- counter + 1
+    //                counter <- counter + 1
                     adj.[i, j] <- false
-    printfn "Removed %d edges" counter
+    //printfn "Removed %d edges" counter
     graph
