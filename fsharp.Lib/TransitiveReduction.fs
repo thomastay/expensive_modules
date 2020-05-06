d5fb73d49632e4996feedb2fb7e58887db251ee5
ttay <ttay@umich.edu>
2020-05-06 12:52:46 +0800

    got timing down to 1.3s

--- fsharp.Lib/TransitiveReduction.fs
+++ fsharp.Lib/TransitiveReduction.fs
@@ -1,4 +1,5 @@
 ﻿module TransitiveReduction
+open System.Threading.Tasks
 open GraphCreation
 
 // Implements the transitive reduction of a Directed Acyclic Graph
@@ -6,15 +7,14 @@ open GraphCreation
 
 // Implements the transitve reduction algorithm
 // First implemented by Gries, D., Martin, A. J. et al (1987)
+// minor perf improvements have been made
 let reduce (graph: Digraph): Digraph =
     let adj = graph.adjacency
-    let high = Array2D.length1 adj - 1
-    let mutable counter = 0
-    for k = high downto 0 do
-        for i in 0..high do
-            for j in 0..high do
-                if adj.[i, k] && adj.[k, j] then
-    //                counter <- counter + 1
-                    adj.[i, j] <- false
-    //printfn "Removed %d edges" counter
+    let n = Array2D.length1 adj
+    for k = n-1 downto 0 do
+        for i in 0..n-1 do
+            if adj.[i, k] then
+                for j in 0..n-1 do
+                    if adj.[k, j] then
+                        adj.[i, j] <- false
     graph
